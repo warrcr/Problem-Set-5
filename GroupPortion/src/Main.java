c8f7c9cf8d97181abd0421937557fb58b60cfdad
jack7dawson <dawsjw20@wfu.edu>
2022-11-28 11:03:08 -0500

    first commit

---
+++ GroupPortion/src/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+// This is the main method of Brick Breaker
+/* Ideas to implement into code:
+1. Extra balls after you win the first time
+2. Add more bricks after they win the first time
+3. Increase speed of ball
+4. Power-Ups (extra balls, extra life, etc.)
+5. Bricks need to be hit multiple times to break
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+
+        Game gamePlay = new Game();
+
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+
+    }
+}
