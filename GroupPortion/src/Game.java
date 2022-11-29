3f907c79c07ce78eae417a9325cfcc69c45d0df5
JuneBernstein <bernjt21@wfu.edu>
2022-11-29 17:43:20 -0500

    changing starting ball position

--- GroupPortion/src/Game.java
+++ GroupPortion/src/Game.java
@@ -13,8 +13,8 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     private Timer timer;
     private int delay = 8;
     private int playerX = 310;
-    private int ballposX = 120;
-    private int ballposY = 350;
+    private int ballposX = 350;
+    private int ballposY = 520;
     private int ballXdir = -1;
     private int ballYdir = -2;
     private LevelMapGenerator map;
